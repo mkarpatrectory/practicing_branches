1885c20011ce214e981090d4f2f31fd7676f811a
EBlancoAtRectory <esteban.blanco@rectorywolves.org>
2024-03-29 11:23:41 -0400

    Changed the code.py file

--- code.py
+++ code.py
@@ -2,8 +2,19 @@ import pandas as pd
 
 print("My boy Aidan P. has unlimited rizz.")
 
-fl = pd.read.csv('customers-1000.csv')
+fl = pd.read_csv('customers-1000.csv')
 
 cols = fl.columns.tolist()
 
-print(cols)
+names = fl[cols[2]].tolist()
+cities = fl[cols[5]].tolist()
+
+
+class facts:
+	def __init__(self,name,city):
+		self.name = name
+		self.city = city
+		
+p1 = facts(names[1],cities[1])
+
+print(p1.name)
