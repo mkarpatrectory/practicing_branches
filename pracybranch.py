fbc8fea3b2d235ce52eb2636b263d90986b63230
aidanpet <aidan.petion@rectorywolves.org>
2024-04-01 11:41:17 -0400

    i added a class for pookie bear esteban

--- pracybranch.py
+++ pracybranch.py
@@ -1,6 +1,23 @@
 print("whats the word slim")
-fl = pd.read.csv ('customer-1000.csv')
+import pandas as pd
 
+fl = pd.read.csv ('customer-1000.csv')
+f = list(fl)
 cols = fl.columns.tolist()
 
-print(cols)
+n = fl[cols[2]].tolist()
+ci = fl[cols[5]].tolist()
+
+
+print(f)
+
+class APsClass:
+	def__init__(self,country):
+		self.country = country
+	def ifChina(self):
+		countries = []
+		for row in clt:
+			if row(6) == "China":
+				countries.append(row)	
+		return countries
+	
