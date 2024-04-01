9ac112cf60296713e7ff22d53a3837d8989f37aa
mafewma1 <zhixing.ma@rectorywolves.org>
2024-04-01 11:38:09 -0400

    Added Class

--- PracBranchCode.py
+++ PracBranchCode.py
@@ -3,6 +3,19 @@ from pandas import *
 
 clt = read_csv('customers-1000.csv')
 f = list(clt)
+print(clt)
 
-print(f[0])
+print(f)
 
+class MyClass:
+	print('bruh')
+	def __init__(self, country):
+		self.country = country
+	def iffromChina(self):
+		countries = []
+		for row in clt:
+			if row[6] == "China":
+				countries.append(row)
+		return countries
+stuff =clt.MyClass()
+print(stuff)
